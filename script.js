e99fd10b597b4742927a8ed4e05b5cfe1e019714
Your Name <tawanswc@gmail.com>
2022-08-05 18:14:19 +0700

    update1

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const todoCtn = document.getElementById("todo-container");
 
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
-
   //your code here
+  else console.log(inputAdd.value);
+  addTodo(inputAdd.value, 0);
 };
 
 function addTodo(title, completed) {
+  console.log("addTodo working");
   //create a div that holds todo title, done button, delete button
   const div = document.createElement("div");
   div.className = "border-bottom p-1 py-2 fs-2 d-flex";
@@ -29,20 +31,38 @@ function addTodo(title, completed) {
   deleteBtn.className = "btn btn-danger";
 
   //your code here
+  if (inputAdd.value === "") {
+    alert("ใส่อะไรไปก่อนกด Enter นะไอ่ต้าว");
+  } else {
+    const list = document.getElementById("todo-container");
+    div.appendChild(span);
+    div.appendChild(doneBtn);
+    div.appendChild(deleteBtn);
+    list.insertBefore(div, list.children[0]);
+
+    document.getElementById("input-add-todo").value = "";
+  }
+
   //append todo to HTML...
   //define buttons event...
+
+  saveTodo();
 }
 
 function saveTodo() {
+  console.log("saveTodo Working");
   const data = [];
   for (const todoDiv of todoCtn.children) {
+    data.push(todoDiv);
     //your code here
   }
   //your code here
+  console.log(data);
 }
 
 function loadTodo() {
   //your code here
+  console.log("load working");
 }
 
 loadTodo();
